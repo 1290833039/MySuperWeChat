6982041f85b11682a2a5666f48edaac74eed1974
xiaotao <1290833039@qq.com>
2016-05-26 16:46:48 +0800

    修改NewGroupActivity的整理代码，添加setListener,initView等方法块的提交

--- src/cn/ucai/superwechat/activity/NewGroupActivity.java
+++ src/cn/ucai/superwechat/activity/NewGroupActivity.java
@@ -28,6 +28,8 @@ import android.widget.Toast;
 import com.easemob.chat.EMGroupManager;
 import com.easemob.exceptions.EaseMobException;
 
+import cn.ucai.superwechat.R;
+
 public class NewGroupActivity extends BaseActivity {
 	private EditText groupNameEditText;
 	private ProgressDialog progressDialog;
@@ -35,6 +37,10 @@ public class NewGroupActivity extends BaseActivity {
 	private CheckBox checkBox;
 	private CheckBox memberCheckbox;
 	private LinearLayout openInviteContainer;
+
+	String avatarName;
+	String st1;
+	String st2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +52,35 @@ public class NewGroupActivity extends BaseActivity {
 
 	private void setListener() {
 		setOnCheckchangedListener();
+		setSaveGroupClickListener();
+		setGroupIconClickListener();
+	}
+
+	private void setGroupIconClickListener() {
+		findViewById(R.id.layout_groups_avatar).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+	}
+
+	private void setSaveGroupClickListener() {
+		findViewById(R.id.saveGroup).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String str6 = getResources().getString(cn.ucai.superwechat.R.string.Group_name_cannot_be_empty);
+				String name = groupNameEditText.getText().toString();
+				if (TextUtils.isEmpty(name)) {
+					Intent intent = new Intent(NewGroupActivity.this, AlertDialog.class);
+					intent.putExtra("msg", str6);
+					startActivity(intent);
+				} else {
+					// 进通讯录选人
+					startActivityForResult(new Intent(NewGroupActivity.this, GroupPickContactsActivity.class).putExtra("groupName", name), 0);
+				}
+			}
+		});
 	}
 
 	private void setOnCheckchangedListener() {
@@ -70,69 +105,64 @@ public class NewGroupActivity extends BaseActivity {
 		openInviteContainer = (LinearLayout) findViewById(cn.ucai.superwechat.R.id.ll_open_invite);
 	}
 
-	/**
-	 * @param v
-	 */
-	public void save(View v) {
-		String str6 = getResources().getString(cn.ucai.superwechat.R.string.Group_name_cannot_be_empty);
-		String name = groupNameEditText.getText().toString();
-		if (TextUtils.isEmpty(name)) {
-			Intent intent = new Intent(this, AlertDialog.class);
-			intent.putExtra("msg", str6);
-			startActivity(intent);
-		} else {
-			// 进通讯录选人
-			startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), 0);
-		}
+	//添加头像文件名方法
+	private String getAvatarName() {
+		avatarName = System.currentTimeMillis()+"";
+		return avatarName;
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		String st1 = getResources().getString(cn.ucai.superwechat.R.string.Is_to_create_a_group_chat);
-		final String st2 = getResources().getString(cn.ucai.superwechat.R.string.Failed_to_create_groups);
+		st1 = getResources().getString(cn.ucai.superwechat.R.string.Is_to_create_a_group_chat);
+		st2 = getResources().getString(cn.ucai.superwechat.R.string.Failed_to_create_groups);
 		if (resultCode == RESULT_OK) {
-			//新建群组
-			progressDialog = new ProgressDialog(this);
-			progressDialog.setMessage(st1);
-			progressDialog.setCanceledOnTouchOutside(false);
-			progressDialog.show();
+			newGroups(data);
 
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					// 调用sdk创建群组方法
-					String groupName = groupNameEditText.getText().toString().trim();
-					String desc = introductionEditText.getText().toString();
-					String[] members = data.getStringArrayExtra("newmembers");
-					try {
-						if(checkBox.isChecked()){
-							//创建公开群，此种方式创建的群，可以自由加入
-							//创建公开群，此种方式创建的群，用户需要申请，等群主同意后才能加入此群
-						    EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
-						}else{
-							//创建不公开群
-						    EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
-						}
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								setResult(RESULT_OK);
-								finish();
-							}
-						});
-					} catch (final EaseMobException e) {
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-							}
-						});
-					}
-
-				}
-			}).start();
 		}
+	}
+
+	private void newGroups(final Intent data) {
+		//新建群组
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage(st1);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.show();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// 调用sdk创建群组方法
+				String groupName = groupNameEditText.getText().toString().trim();
+				String desc = introductionEditText.getText().toString();
+				String[] members = data.getStringArrayExtra("newmembers");
+				try {
+					if(checkBox.isChecked()){
+						//创建公开群，此种方式创建的群，可以自由加入
+						//创建公开群，此种方式创建的群，用户需要申请，等群主同意后才能加入此群
+						EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
+					}else{
+						//创建不公开群
+						EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
+					}
+					runOnUiThread(new Runnable() {
+						public void run() {
+							progressDialog.dismiss();
+							setResult(RESULT_OK);
+							finish();
+						}
+					});
+				} catch (final EaseMobException e) {
+					runOnUiThread(new Runnable() {
+						public void run() {
+							progressDialog.dismiss();
+							Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+						}
+					});
+				}
+
+			}
+		}).start();
 	}
 
 	public void back(View view) {
