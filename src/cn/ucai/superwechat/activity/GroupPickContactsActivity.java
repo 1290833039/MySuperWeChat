dbc39cb3c32ef53a22375ed6f68a5eb4edd0fd21
xiaotao <1290833039@qq.com>
2016-05-27 08:48:19 +0800

    修改GroupPickContactsActivity中获取要被添加的成员getToBeAddMembers和确认选择的members方法的提交

--- src/cn/ucai/superwechat/activity/GroupPickContactsActivity.java
+++ src/cn/ucai/superwechat/activity/GroupPickContactsActivity.java
@@ -39,6 +39,7 @@ import cn.ucai.superwechat.DemoHXSDKHelper;
 import cn.ucai.superwechat.adapter.ContactAdapter;
 import cn.ucai.superwechat.bean.Contact;
 import cn.ucai.superwechat.domain.User;
+import cn.ucai.superwechat.utils.Utils;
 import cn.ucai.superwechat.widget.Sidebar;
 
 public class GroupPickContactsActivity extends BaseActivity {
@@ -102,30 +103,33 @@ public class GroupPickContactsActivity extends BaseActivity {
 
 	/**
 	 * 确认选择的members
-	 * 
+	 *
 	 * @param v
 	 */
 	public void save(View v) {
-		setResult(RESULT_OK, new Intent().putExtra("newmembers", getToBeAddMembers().toArray(new String[0])));
+		setResult(RESULT_OK, new Intent().putExtra("newmembers", getToBeAddMembers()));
 		finish();
 	}
 
 	/**
 	 * 获取要被添加的成员
-	 * 
 	 * @return
 	 */
-	private List<String> getToBeAddMembers() {
-		List<String> members = new ArrayList<String>();
+	private Contact[] getToBeAddMembers() {
+		Contact[] contacts = new Contact[0];
 		int length = contactAdapter.isCheckedArray.length;
+
 		for (int i = 0; i < length; i++) {
-			String username = contactAdapter.getItem(i).getMContactCname();
-			if (contactAdapter.isCheckedArray[i] && !exitingMembers.contains(username)) {
-				members.add(username);
+			Contact contact = contactAdapter.getItem(i);
+			if (contactAdapter.isCheckedArray[i]
+					&& !exitingMembers.contains(contact.getMContactCname())) {
+				contacts = Utils.add(contacts,contact);
 			}
 		}
-
-		return members;
+		if (contacts.length>0){
+			return contacts;
+		}
+		return null;
 	}
 
 	/**
